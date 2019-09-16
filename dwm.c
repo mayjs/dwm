f36071784c47bef27a5d9d44d760d8bb166b1787
may <johannes.may@udo.edu>
2019-09-16 23:24:29 +0200

    Switch to focused windows

--- dwm.c
+++ dwm.c
@@ -625,8 +625,14 @@ clientmessage(XEvent *e)
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
 				|| (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->isfullscreen)));
 	} else if (cme->message_type == netatom[NetActiveWindow]) {
-		if (c != selmon->sel && !c->isurgent)
-			seturgent(c, 1);
+		if (c != selmon->sel && !c->isurgent) {
+    		// TODO: Special rules where we dont autoswitch (e.g. floating windows)
+    		Arg arg;
+    		arg.ui = c->tags;
+    		view(&arg);
+    		focus(c);
+			// seturgent(c, 1);
+		}
 	}
 }
 
